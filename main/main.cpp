7db64ad888645c19e121c1a26b703923cadbd29d
Oyalmli <oyvind.almli@gmail.com>
2022-10-03 21:27:45 +0200

    Updated main example

--- main/main.cpp
+++ main/main.cpp
@@ -3,7 +3,7 @@
 using namespace dfl;
 
 extern "C" void app_main(void) {
-    gen::range(10)
+    mod::take(100, gen::counter<int32_t>())
     >>= pipe::transform([](int i){ return i + 1; })
     >>= sink::printf("%d\n");
 }
